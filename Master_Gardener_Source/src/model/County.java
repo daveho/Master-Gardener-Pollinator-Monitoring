a444514027b9e2f0d8b94bc63f1ace7214145d34
Alex <abrown96@ycp.edu>
2017-10-20 16:18:47 -0400

    Revert "3 tables working and import from csv"
    This reverts commit 806fcd6eada5b4e51a1b55f984a8f4258994d96a.

--- Master_Gardener_Source/src/model/County.java
+++ Master_Gardener_Source/src/model/County.java
@@ -7,11 +7,19 @@ public class County
 	// Alex - There's a good chance we simply won't use stateName. That's acceptable.
 	private String countyName, stateName;
 	
-	public County()
+	public County(int countyID, String countyName, String stateName)
 	{
-		this.countyName= "";
-		this.stateName = "";
-		
+	    if(countyID <= 0)
+        {
+            throw new IllegalArgumentException("County ID must be a non-zero, positive integer.");
+        }
+        if(countyName.equals("") || stateName.equals(""))
+        {
+            throw new IllegalArgumentException("Either county name or state name are empty. These are required fields.");
+        }
+        this.countyID = countyID;
+		this.countyName = countyName;
+		this.stateName = stateName;
 	}
 	
 	public void setCountyID(int countyID)
--- Master_Gardener_Source/src/model/Pollinator.java
+++ Master_Gardener_Source/src/model/Pollinator.java
@@ -13,14 +13,26 @@ public class Pollinator
 	private String pollinatorName;
 	private String pollinatorType;
 	
-	// This might not be necessary, therefore it's documented here in the event that it is.
+	// This variable (below) might not be necessary, therefore it's documented here in the event that it is.
 	// It doesn't seem like it will be though. Don't take my word on that. You don't know who wrote 
 	// this comment. Go away.
 	//private int weekNumber;
 	
-	public Pollinator()
+	public Pollinator(int pollinator_id, String pollinator_name, String pollinator_type)
 	{
-		
+		if(pollinator_id <= 0)
+		{
+			throw new IllegalArgumentException("Pollinator ID must be a non-zero positive integer.");
+		}
+
+		if(pollinator_name.equals("") || pollinator_type.equals(""))
+		{
+			throw new IllegalArgumentException("Pollinator Name/Type are required fields.");
+		}
+
+		this.pollinatorID = pollinator_id;
+		this.pollinatorName = pollinator_name;
+		this.pollinatorType = pollinator_type;
 	}
 	
 	public void setPollinatorID(int pollinatorid)
@@ -44,7 +56,7 @@ public class Pollinator
 	{
 		return this.pollinatorName;
 	}
-	public String geetPollinatorType()
+	public String getPollinatorType()
 	{
 		return this.pollinatorType;
 	}
